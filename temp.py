c98497cbe3f871651a33335cf688fc86514578e9
Aditya Surve <aditya@Adityas-MacBook-Pro.local>
2024-10-21 13:56:25 +0530

    Commit from mac

---
+++ temp.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+#Write a function to add two numbers
+def add_numbers(num1, num2):
+    return num1 + num2
+
+#Call the function with two numbers
+result = add_numbers(5, 10)
+print(result)
+
